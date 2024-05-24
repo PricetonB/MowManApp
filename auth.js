e240070610c59ad2508f606916d0bfbfd631bfe5
Priceton Braswell <pricetonbraswell@gmail.com>
2024-05-24 03:04:02 -0400

    automating urls

--- auth.js
+++ auth.js
@@ -10,14 +10,14 @@ GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 
 
-
+const baseUrl = 'http://localhost:3000';
 
 
 
 passport.use(new GoogleStrategy({
   clientID: GOOGLE_CLIENT_ID,
   clientSecret: GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/google/callback",
+  callbackURL: `${baseUrl}/google/callback`,
   passReqToCallback: true
 },
 function(request, accessToken, refreshToken, profile, done) {
